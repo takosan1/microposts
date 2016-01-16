86f3ebdf8bcbad5fefef4d5d34f89be97930d2ef
takosan1 <takosan6472gk@gmail.com>
2016-01-16 07:56:26 +0000

    followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
     
     
     has_many :follower_relationships, class_name: "Relationship",
-                                       foreign_key: "follower_id",
+                                       foreign_key: "followed_id",
                                        dependent:   :destroy
     has_many :follower_users, through: :follower_relationships, source: :follower
     
